ce0f3d7653137fdbf0825dddaf41c041a5e9d492
lupacescueduard <lupacescueduard@yahoo.com>
2018-08-17 17:16:33 +0300

    Paper initialize

---
+++ CONTRIBUTING.md
@@ -0,0 +1,9 @@
+* Fork the repository
+* Clone it on your PC
+* `npm install` or `yarn install`
+* Make changes, commit open PR
+
+### Notes
+* Please don't use jQuery or jQuery based plugins since there are many pure Vue alternatives
+
+This project uses [vue-cli 3](https://github.com/vuejs/vue-cli).
---
+++ src/plugins/paperDashboard.js
@@ -0,0 +1,19 @@
+import Notify from "vue-notifyjs";
+import SideBar from "@/components/SidebarPlugin";
+import GlobalComponents from "./globalComponents";
+import GlobalDirectives from "./globalDirectives";
+import "es6-promise/auto";
+
+//css assets
+import "bootstrap/dist/css/bootstrap.css";
+import "@/assets/sass/paper-dashboard.scss";
+import "@/assets/css/themify-icons.css";
+
+export default {
+  install(Vue) {
+    Vue.use(GlobalComponents);
+    Vue.use(GlobalDirectives);
+    Vue.use(SideBar);
+    Vue.use(Notify);
+  }
+}
