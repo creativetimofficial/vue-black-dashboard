a8cb1967b09372ebbf6b08581abfa9f2607735b2
cristij <joracristi@gmail.com>
2018-09-02 14:34:19 +0300

    Fix rtl stylesheet

--- src/plugins/RTLPlugin.js
+++ src/plugins/RTLPlugin.js
@@ -14,11 +14,22 @@ export default {
           this.isRTL = true;
           this.getDocClasses().add('rtl');
           this.getDocClasses().add('menu-on-right');
+          this.toggleBootstrapRTL(true);
         },
         disableRTL() {
           this.isRTL = false;
           this.getDocClasses().remove('rtl');
           this.getDocClasses().remove('menu-on-right');
+          this.toggleBootstrapRTL(false);
+        },
+        toggleBootstrapRTL(value) {
+          for (let i=0; i < document.styleSheets.length; i++){
+            let styleSheet = document.styleSheets[i];
+            let { href } = styleSheet;
+            if(href && href.endsWith('bootstrap-rtl.css')){
+              styleSheet.disabled = !value;
+            }
+          }
         }
       }
     });
