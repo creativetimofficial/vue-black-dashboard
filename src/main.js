d24fe30b7e0d7127e9d08c09aa2f5750fedc6d1a
cristij <joracristi@gmail.com>
2018-09-02 15:46:42 +0300

    Add i18n plugin

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from "./App";
 import router from "./router/index";
 
 import BlackDashboard from "./plugins/blackDashboard";
+import i18n from './i18n'
 Vue.use(BlackDashboard);
 
 /* eslint-disable no-new */
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 };
