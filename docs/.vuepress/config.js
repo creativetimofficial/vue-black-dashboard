97243b3cdd2904e36ceebe36ea65f75d525928ec
cristij <joracristi@gmail.com>
2018-10-07 15:05:05 +0300

    Initial documentation version (not working yet)

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,72 @@
+const markdownParser = require('./markdownParser')
+const path = require('path');
+
+module.exports = {
+  base: process.env.NODE_ENV ? '/vue-argon-design-system/documentation/' : '',
+  markdown: {
+    config: markdownParser
+  },
+  configureWebpack: (config, isServer) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../../src')
+    config.resolve.alias['assets'] = path.resolve(__dirname, '../../src/assets')
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+          options.optimizeSSR = false
+          return options;
+        }
+      )
+  },
+  head: [
+    ['link', { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" }],
+  ],
+  themeConfig: {
+    sidebarDepth: 1,
+
+    sidebar: [{
+      title: 'Getting Started',
+      collapsable: false,
+      children: [
+        '/',
+        '/quick-start',
+        '/licence',
+        '/contents',
+        '/build-tools'
+      ]
+    },
+    {
+      title: 'Components',
+      collapsable: false,
+      children: [
+        '/components/alerts',
+        '/components/badges',
+        '/components/buttons',
+        '/components/carousel',
+        '/components/dropdowns',
+        '/components/forms',
+        '/components/modal',
+        '/components/tabs',
+        '/components/navbar',
+        '/components/pagination',
+        '/components/popovers',
+        '/components/progress',
+        '/components/tooltips',
+      ]
+    },
+    {
+      title: 'Plugins',
+      collapsable: false,
+      children: [
+        // '/components/plugin_datepicker',
+        // '/components/plugin_slider',
+      ]
+    }
+    ],
+    nav: [
+      { text: 'Home', link: '/' },
+    ]
+  }
+};
---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,23 @@
+import DemoBlock from './demo-block/demo-block'
+import * as Components from '@/components/index'
+import Dashboard from '@/plugins/blackDashboard'
+import './doc_styles.scss'
+import './docs.css'
+import getElements from './utils/get-sidebar-elements';
+
+export default ({
+                  Vue, // the version of Vue being used in the VuePress app
+                  options, // the options for the root Vue instance
+                  router, // the router instance for the app
+                  siteData
+                }) => {
+  Vue.use(Dashboard)
+  Vue.component('demo-block', DemoBlock);
+  let componentEntries = Object.entries(Components);
+  for(let [name, component] of componentEntries) {
+    Vue.component(component.name || name, component)
+  }
+  Vue.prototype.$docs = Object.values(Components);
+  let docComponents = getElements(componentEntries);
+  siteData.themeConfig.sidebar = siteData.themeConfig.sidebar.concat(docComponents);
+}
