821b706968081e4bc480be771d4071274f19356a
cristij <joracristi@gmail.com>
2018-10-07 22:39:48 +0300

    Add base url for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
+const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   lintOnSave: false,
-
+  baseUrl: isProd ? "/vue-black-dashboard/" : "",
   pluginOptions: {
     i18n: {
       locale: 'en',
