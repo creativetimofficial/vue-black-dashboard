15b46a2e13041433f7246acb3794c1b058c204e8
cristij <joracristi@gmail.com>
2018-10-18 16:31:22 +0300

    Fix Sidebar menu on mobile RTL Fix collapsable menu on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <div :class="{'nav-open': $sidebar.showSidebar}">
+  <div>
     <notifications></notifications>
     <router-view :key="$route.fullPath"></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    disableRTL() {
-      if(!this.$rtl.isRTL) {
-        this.$rtl.disableRTL();
+  export default {
+    methods: {
+      disableRTL() {
+        if (!this.$rtl.isRTL) {
+          this.$rtl.disableRTL();
+        }
+      },
+      toggleNavOpen() {
+        let root = document.getElementsByTagName('html')[0];
+        root.classList.toggle('nav-open');
       }
+    },
+    mounted() {
+      this.$watch('$route', this.disableRTL, { immediate: true });
+      this.$watch('$sidebar.showSidebar', this.toggleNavOpen)
     }
-  },
-  mounted() {
-    this.$watch('$route', this.disableRTL, {immediate: true});
-  }
-};
+  };
 </script>
 
 <style lang="scss"></style>
