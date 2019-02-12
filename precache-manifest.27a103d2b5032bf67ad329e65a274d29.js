0d3169a57e546060072ad2791c5682cb3a4ddb9d
cristij <joracristi@gmail.com>
2019-02-12 14:14:25 +0200

    Updates

---
+++ precache-manifest.27a103d2b5032bf67ad329e65a274d29.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/vue-black-dashboard/favicon.png"
+  },
+  {
+    "revision": "84d001163bd86d3bf8f9cc117b316521",
+    "url": "/vue-black-dashboard/img/james.jpg"
+  },
+  {
+    "revision": "4ec805b26cd469c628ed056227ff3df1",
+    "url": "/vue-black-dashboard/img/mike.jpg"
+  },
+  {
+    "revision": "ecb6b8c6ad5c96b414e1",
+    "url": "/vue-black-dashboard/js/chunk-vendors.cec82c63.js"
+  },
+  {
+    "revision": "661ce90d810b7be437df",
+    "url": "/vue-black-dashboard/js/common.50849ae4.js"
+  },
+  {
+    "revision": "13c65d169bec44556079",
+    "url": "/vue-black-dashboard/js/dashboard.3296137f.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-black-dashboard/robots.txt"
+  },
+  {
+    "revision": "35c003af6869e94ebca5",
+    "url": "/vue-black-dashboard/js/app.325ef351.js"
+  },
+  {
+    "revision": "24f0309430d149fcda41da1024043c2f",
+    "url": "/vue-black-dashboard/index.html"
+  },
+  {
+    "revision": "a30e21b28cb8bfddda9d33d5a5e4d28b",
+    "url": "/vue-black-dashboard/img/emilyz.jpg"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/vue-black-dashboard/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "0dbfc9d41dc7819055eb0cfb608bed0d",
+    "url": "/vue-black-dashboard/img/img_3115.jpg"
+  },
+  {
+    "revision": "49458d8716dcfc74c2e95064dbb125d2",
+    "url": "/vue-black-dashboard/img/header.jpg"
+  },
+  {
+    "revision": "53142ad3b97e5a15b4b08525816830e3",
+    "url": "/vue-black-dashboard/img/anime6.png"
+  },
+  {
+    "revision": "024b8d2cfdeb22410927a6c67eb877bf",
+    "url": "/vue-black-dashboard/img/anime3.png"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "/vue-black-dashboard/img/default-avatar.png"
+  },
+  {
+    "revision": "f8624a716c4efc245ad1550c657b5f98",
+    "url": "/vue-black-dashboard/img/apple-icon.png"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/vue-black-dashboard/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "7b8a6099ce9bdadcf1bcfde26a139275",
+    "url": "/vue-black-dashboard/img/favicon.png"
+  },
+  {
+    "revision": "f68d9108445df3450b1eed7ae0256fc1",
+    "url": "/vue-black-dashboard/img/bg5.jpg"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/vue-black-dashboard/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/vue-black-dashboard/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "ecb6b8c6ad5c96b414e1",
+    "url": "/vue-black-dashboard/css/chunk-vendors.45b9d7f5.css"
+  },
+  {
+    "revision": "35c003af6869e94ebca5",
+    "url": "/vue-black-dashboard/css/app.9698f825.css"
+  },
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "/vue-black-dashboard/apple-icon.png"
+  }
+];
