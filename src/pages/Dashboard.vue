640cfae3f0113a8c9d021e466652aa0ae09bc127
Toma Rares <tomairares@gmail.com>
2020-06-26 11:12:33 +0300

    Refactor(charts): Fix error

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -150,7 +150,10 @@
             [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
           ],
           activeIndex: 0,
-          chartData: null,
+          chartData: {
+            datasets: [{ }],
+            labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+          },
           extraOptions: chartConfigs.purpleChartOptions,
           gradientColors: config.colors.primaryGradient,
           gradientStops: [1, 0.4, 0],
