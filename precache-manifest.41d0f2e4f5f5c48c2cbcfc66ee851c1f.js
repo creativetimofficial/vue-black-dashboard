781aa0beca01ef40db5dbbb13d831ffae72a1337
Toma Rares <tomairares@gmail.com>
2020-06-26 13:23:31 +0300

    Updates

---
+++ precache-manifest.41d0f2e4f5f5c48c2cbcfc66ee851c1f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "/vue-black-dashboard/apple-icon.png"
+  },
+  {
+    "revision": "a478113add7d0e0770e1",
+    "url": "/vue-black-dashboard/css/app.73777243.css"
+  },
+  {
+    "revision": "eeefc5690acb326f8ba2",
+    "url": "/vue-black-dashboard/css/chunk-vendors.70978615.css"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/vue-black-dashboard/favicon.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/vue-black-dashboard/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/vue-black-dashboard/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/vue-black-dashboard/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/vue-black-dashboard/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "3b91359dcf1cf96c8440a7532ac61dda",
+    "url": "/vue-black-dashboard/img/anime3.png"
+  },
+  {
+    "revision": "2a5a031c63d290c6cd3803e21d8c3b5d",
+    "url": "/vue-black-dashboard/img/anime6.png"
+  },
+  {
+    "revision": "d2caaad36a395104bd99f30487798d79",
+    "url": "/vue-black-dashboard/img/apple-icon.png"
+  },
+  {
+    "revision": "9b0c49b657dd738a77a37e7dabcc0b5d",
+    "url": "/vue-black-dashboard/img/bg5.jpg"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "/vue-black-dashboard/img/default-avatar.png"
+  },
+  {
+    "revision": "fadc3f3a4c94b151d8b8ed33456c2344",
+    "url": "/vue-black-dashboard/img/emilyz.jpg"
+  },
+  {
+    "revision": "4422794dfa779751326660ae681b082e",
+    "url": "/vue-black-dashboard/img/favicon.png"
+  },
+  {
+    "revision": "f220d1815d421bfa192ee352780ba194",
+    "url": "/vue-black-dashboard/img/img_3115.jpg"
+  },
+  {
+    "revision": "40fdb6e1e72e21ff904da682c46d0116",
+    "url": "/vue-black-dashboard/img/james.jpg"
+  },
+  {
+    "revision": "93f1328bd264f02aacef8b0c6b498445",
+    "url": "/vue-black-dashboard/img/mike.jpg"
+  },
+  {
+    "revision": "479c2ae1c169915dad7ece0e4cca3341",
+    "url": "/vue-black-dashboard/index.html"
+  },
+  {
+    "revision": "a478113add7d0e0770e1",
+    "url": "/vue-black-dashboard/js/app.6d6e3316.js"
+  },
+  {
+    "revision": "eeefc5690acb326f8ba2",
+    "url": "/vue-black-dashboard/js/chunk-vendors.cb32cb64.js"
+  },
+  {
+    "revision": "2c3f4a0e0474c447951f",
+    "url": "/vue-black-dashboard/js/common.c4e57fa7.js"
+  },
+  {
+    "revision": "470ce87d25f5086b8d6a",
+    "url": "/vue-black-dashboard/js/dashboard.7f4562d2.js"
+  },
+  {
+    "revision": "1a167cfd82a1a7480ea601fccc8d7e18",
+    "url": "/vue-black-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-black-dashboard/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vue-black-dashboard/precache-manifest.584bca469a697af58fa204e301b9c408.js"
+  "/vue-black-dashboard/precache-manifest.41d0f2e4f5f5c48c2cbcfc66ee851c1f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-black-dashboard"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "vue-black-dashboard"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
